e63a8add18b212da8767f216a5618f2e91f0d090
realaravinth <realaravinth@batsense.net>
2020-12-15 15:13:53 +0530

    grid

---
+++ dec-2020-lightning-talks/index.js
@@ -0,0 +1,25 @@
+const drawGrid = () => {
+  let divContainer = document.getElementById('grid');
+  let columns = 50;
+  let rows = 13;
+
+  for (let i = 1; i <= columns; i++) {
+    for (let j = 1; j <= rows; j++) {
+      let box = document.createElement('div');
+
+      box.id = `grid-box-${i}`;
+      box.className = 'grid-box';
+      box.style = `grid-column:${i}; grid-row: ${j};`;
+      if (i == 1) {
+        box.className += ' grid-box-initial';
+      };
+      if (i == columns) {
+        box.className += ' grid-box-final';
+      };
+
+      divContainer.appendChild(box);
+    }
+  }
+}
+
+drawGrid();
